68f83a3b229113683b0452f85a52951d7048da0c
Mustafa <79559036+SirMustafa@users.noreply.github.com>
2025-05-14 21:11:02 +0300

    Player interactions

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -6,29 +6,45 @@ public class Bomb : MonoBehaviour
 {
     [Header("Explosion Settings")]
     public int range = 5;
+    public float delayBeforeExplode = 5f;
+
     public LayerMask blowableLayer;
     public float tileSize = 1f;
 
+    private static readonly Vector3[] directions = new Vector3[]
+    {
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back
+    };
+
     private void OnEnable()
     {
-        //this.transform.DOLocalMoveY(1, 1).SetLoops(-1, LoopType.Yoyo);
+        transform.DOLocalMoveY(1, 1).SetLoops(-1, LoopType.Yoyo);
+        ExplodeAfterDelay().Forget();
+    }
+
+    private async UniTaskVoid ExplodeAfterDelay()
+    {
+        await UniTask.Delay((int)(delayBeforeExplode * 1000));
         ExplodeGrid();
+        transform.DOKill();
+        Destroy(gameObject);
     }
 
     private void ExplodeGrid()
     {
         Vector3 origin = transform.position;
-        TryExplodeDirection(Vector3.right);
-        TryExplodeDirection(Vector3.left);
-        TryExplodeDirection(Vector3.forward);
-        TryExplodeDirection(Vector3.back);
-        //transform.DOKill();
+
+        foreach (var dir in directions)
+        {
+            TryExplodeDirection(origin, dir);
+        }
     }
 
-    private void TryExplodeDirection(Vector3 dir)
+    private void TryExplodeDirection(Vector3 origin, Vector3 dir)
     {
-        Vector3 origin = transform.position;
-
         for (int step = 1; step <= range; step++)
         {
             Vector3 checkPos = origin + dir * step * tileSize;
