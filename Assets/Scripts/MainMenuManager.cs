6876a1f65281418be0f00f0aae23ebdac6f5b8f2
Mustafa <79559036+SirMustafa@users.noreply.github.com>
2025-05-17 00:38:00 +0300

    ui update

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,27 +1,45 @@
+using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public static MainMenuManager Instance;
+
     [SerializeField] List<GameObject> Panels = new List<GameObject>();
+    List<CanvasGroup> PanelGroups = new List<CanvasGroup>();
     int previousPanelIndex = 0;
+    [SerializeField] float fadeDuration = 0.5f;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
         Application.targetFrameRate = 60;
-        Panels[0].SetActive(true);
-    }
 
-    public void StartGame()
-    {
-        SceneManager.LoadScene(1);
+        foreach (GameObject panel in Panels)
+        {
+            PanelGroups.Add(panel.GetComponent<CanvasGroup>());
+        }
+
+        ChangePanel(0);
     }
 
     public void ChangePanel(int panelIndex)
     {
-        Panels[previousPanelIndex].SetActive(false);
-        Panels[panelIndex].SetActive(true);
-        previousPanelIndex = panelIndex;
+        PanelGroups[previousPanelIndex]
+        .DOFade(0f, fadeDuration)
+        .OnComplete(() =>
+        {
+            Panels[previousPanelIndex].SetActive(false);
+            Panels[panelIndex].SetActive(true);
+            PanelGroups[panelIndex].alpha = 0f;
+            PanelGroups[panelIndex].DOFade(1f, fadeDuration);
+            previousPanelIndex = panelIndex;
+        });
     }
 }
---
+++ Assets/Scripts/SetPlayers.cs
@@ -0,0 +1,114 @@
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class SetPlayers : MonoBehaviour
+{
+    [Header("UI Panels and Groups")]
+    [SerializeField] private List<GameObject> Panels = new List<GameObject>();
+    [SerializeField] private List<CanvasGroup> PanelGroups = new List<CanvasGroup>();
+
+    [Header("Player Positions and Indicators")]
+    [SerializeField] private List<RectTransform> PlayersPos = new List<RectTransform>();
+    [SerializeField] private RectTransform player1Indicator;
+    [SerializeField] private RectTransform player2Indicator;
+    [SerializeField] private float fadeDuration = 0.5f;
+
+    private int currentPanelIndex = 0;
+    private int player1Index = 0;
+    private int player2Index = 1;
+
+    private bool isPlayer1Locked = false;
+    private bool isPlayer2Locked = false;
+
+    private float inputThreshold = 0.5f;
+
+    private void Start()
+    {
+        player1Indicator.anchoredPosition = PlayersPos[player1Index].anchoredPosition;
+        player2Indicator.anchoredPosition = PlayersPos[player2Index].anchoredPosition;
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && !isPlayer1Locked)
+        {
+            Vector2 input = ctx.ReadValue<Vector2>();
+            HandleMove(ref player1Index, input.x, player2Index, player1Indicator);
+        }
+    }
+
+    public void OnMove1(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed && !isPlayer2Locked)
+        {
+            Vector2 input = ctx.ReadValue<Vector2>();
+            HandleMove(ref player2Index, input.x, player1Index, player2Indicator);
+        }
+    }
+
+    public void OnApply(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            isPlayer1Locked = true;
+            CheckIfBothPlayersLocked();
+        }
+    }
+
+    public void OnApply1(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            isPlayer2Locked = true;
+            CheckIfBothPlayersLocked();
+        }
+    }
+
+    public void SetPlayerCount(int howManyPlayer)
+    {
+        PanelGroups[currentPanelIndex]
+        .DOFade(0f, fadeDuration)
+        .OnComplete(() =>
+        {
+            Panels[0].SetActive(false);
+            Panels[1].SetActive(true);
+            PanelGroups[1].alpha = 0f;
+            PanelGroups[1].DOFade(1f, fadeDuration);
+        });
+    }
+
+    private void HandleMove(ref int playerIndex, float horizontalInput, int otherPlayerIndex, RectTransform indicator)
+    {
+        if (Mathf.Abs(horizontalInput) < inputThreshold) return;
+
+        int direction = horizontalInput > 0 ? 1 : -1;
+        int tentativeIndex = Mathf.Clamp(playerIndex + direction, 0, PlayersPos.Count - 1);
+
+        if (tentativeIndex == otherPlayerIndex)
+        {
+            int skipIndex = Mathf.Clamp(tentativeIndex + direction, 0, PlayersPos.Count - 1);
+            if (skipIndex != otherPlayerIndex && skipIndex != playerIndex)
+            {
+                tentativeIndex = skipIndex;
+            }
+            else return;
+        }
+
+        if (tentativeIndex != playerIndex)
+        {
+            playerIndex = tentativeIndex;
+            Vector2 targetPos = PlayersPos[playerIndex].anchoredPosition;
+            indicator.DOAnchorPos(targetPos, 0.2f);
+        }
+    }
+    private void CheckIfBothPlayersLocked()
+    {
+        if (isPlayer1Locked && isPlayer2Locked)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
